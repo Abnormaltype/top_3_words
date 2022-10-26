b681f76e15fa27a3b242d08e9873f248b547c656
Alex Zhidkov <sashazudkov@gmail.com>
2022-10-26 09:54:18 +0300

    update

--- main.rb
+++ main.rb
@@ -1,18 +1,18 @@
-def english_words(str)
+def delete_unnecessary_words(str)
   str.downcase.delete('\'').gsub('#?!.,/\\|',' ').split
 end
 
-def element_counts(ary)
+def words_counts(ary)
   ary.group_by { |e| e }.inject({}) { |a, e| a.merge(e[0] => e[1].size) }
 end
 
-def most_common(ary)
+def most_common_words(ary)
   ary.empty? ? nil : 
-    element_counts(ary)
+    words_counts(ary)
       .sort_by { |k, v| v }.last(3)
 end
 
-puts most_common(english_words("n a village of La Mancha, the name of which I have no desire to call to
+puts most_common_words(delete_unnecessary_words("n a village of La Mancha, the name of which I have no desire to call to
   mind, there lived not long since one of those gentlemen that keep a lance
   in the lance-rack, an old buckler, a lean hack, and a greyhound for
   coursing. An olla of rather more beef than mutton, a salad on most
